7612766f491562b1aca01f8529a7be6bbb11c41d
K-Joshua <saintangel9989@gmail.com>
2025-03-26 09:06:13 +0800

    Changes Unit Test Push

---
+++ UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibraryBasicInfo;
+
+namespace LibraryBasicInfo.Tests
+{
+    [TestClass]
+    public class BasicInfoTests
+    {
+
+        [TestMethod]
+        public void Testing_Library()
+        {
+            var info = new BasicInfo()
+            {
+                FirstName = "Kanade",
+                LastName = "Yoisaki",
+                MiddleName = "Airi",
+                Country = "Japan",
+                City = "Tokyo",
+                Street = "Sekai Street",
+                Barangay = "Shibuya",
+                HouseNumber = 39
+            };
+
+            Assert.AreEqual("Kanade", info.FirstName);
+            Assert.AreEqual("Yoisaki", info.LastName);
+            Assert.AreEqual("Airi", info.MiddleName);
+            Assert.AreEqual('A', info.MiddleInitial);
+            Assert.AreEqual("Japan", info.Country);
+            Assert.AreEqual("Tokyo", info.City);
+            Assert.AreEqual("Sekai Street", info.Street);
+            Assert.AreEqual("Shibuya", info.Barangay);
+            Assert.AreEqual(39, info.HouseNumber);
+        }
+
+        [TestMethod]
+        public void AgeCalculation_KanadeYoisaki()
+        {
+            int age = BasicInfo.AgeCalculation(17, 3, 2025, 0, 2, 2005, 10);
+
+            Assert.AreEqual(20, age); 
+        }
+    }
+}
